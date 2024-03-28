8c765ef7cdee67c35372636d92a19aae3750df84
IvanShashkov <ivans.shashkovv@gmail.com>
2024-03-28 16:33:19 +0400

    main update

--- back/api/index.js
+++ back/api/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const routes = require('./routes/index.js')
-const apiRoutes = require('./routes/api')
+const routes = require('../routes')
+const apiRoutes = require('../routes/api')
 
 require('dotenv').config()
 
