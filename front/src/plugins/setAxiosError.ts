c228e04ca4b80f8b599d58c9fcaf4f01233d038e
IvanShashkov <ivans.shashkovv@gmail.com>
2024-04-02 17:46:23 +0400

    main update

--- front/src/plugins/setAxiosError.ts
+++ front/src/plugins/setAxiosError.ts
@@ -4,12 +4,12 @@ import { t } from "i18next"
 const setAxiosError = (error: any) => {
 
     if (!error?.response?.data.message) {
-        return enqueueSnackbar(`${t(error.message)}`, {
+        return enqueueSnackbar(`${error.message}`, {
             variant: 'error',
         })
     }
 
-    enqueueSnackbar(`${error.response.data.message}`, {
+    enqueueSnackbar(`${t(error.response.data.message)}`, {
         variant: 'error',
     })
 }
